d87478f81f09ac75a918361140d6807b7afb52b7
Felipe Saint-jean <fsaint@Felipes-Mac-Studio.local>
2023-11-15 09:45:43 -0800

    Missing extracted file app.js

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const socketIo = require('socket.io');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = socketIo(server);
+
+module.exports = { app, io, server };
+
